7f90d9f815b5b1b998f9b516e9ae62752370d471
dl <dlhomura@gmail.com>
2024-10-29 17:06:38 +0900

    Project 시작

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,16 @@
+import ThemeButton from "./ThemeButton";
+
+const Header = () => {
+    return (
+        <header className="flex">
+            <div>
+                <span>009</span>
+            </div>
+            <div>
+                <ThemeButton />
+            </div>
+        </header>
+    );
+};
+
+export default Header;
