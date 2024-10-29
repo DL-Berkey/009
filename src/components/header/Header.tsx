f1654d24d503ad5aa6abfba99a1dcbc225725e16
dl <dlhomura@gmail.com>
2024-10-29 17:28:22 +0900

    Header Component 추가 - shadcn button component 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,13 +1,23 @@
+import Link from "next/link";
+
+import { Button } from "../ui/button";
+
 import ThemeButton from "./ThemeButton";
 
 const Header = () => {
     return (
-        <header className="flex">
-            <div>
-                <span>009</span>
+        <header className="flex items-center justify-between mx-16 my-4">
+            <div className="text-4xl">
+                <Link href="/">
+                    <span>00</span>
+                    <span className="text-main">9</span>
+                </Link>
             </div>
-            <div>
+            <div className="flex items-center gap-4">
                 <ThemeButton />
+                <Button>
+                    <Link href="/">로그인</Link>
+                </Button>
             </div>
         </header>
     );
--- src/components/header/ThemeButton.tsx
+++ src/components/header/ThemeButton.tsx
@@ -30,7 +30,7 @@ const ThemeButton = () => {
             {/* 모바일 화면이 아닐 때의 UI */}
             <label
                 htmlFor="switch"
-                className="relative items-center justify-between hidden w-12 h-6 bg-gray-200 rounded shadow-inner dark:bg-gray-500 md:flex"
+                className="relative items-center justify-between hidden w-12 h-6 bg-gray-200 rounded shadow-inner dark:bg-gray-500 md:flex hover:cursor-pointer"
             >
                 <input
                     defaultChecked={resolvedTheme === "dark"}
@@ -45,7 +45,7 @@ const ThemeButton = () => {
                 />
                 <Sun className="text-orange-600 scale-75" />
                 <Moon className="text-yellow-400 scale-75" />
-                <span className="absolute w-1/2 h-full transition-all duration-500 translate-x-full -translate-y-1/2 bg-blue-400 rounded peer-checked:translate-x-0 top-1/2"></span>
+                <span className="absolute w-1/2 h-full transition-all duration-500 translate-x-full -translate-y-1/2 bg-blue-500 rounded peer-checked:translate-x-0 top-1/2"></span>
             </label>
         </>
     );
